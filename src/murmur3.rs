3f0506bebe4e3b3a9f17c129a239146e0bacb868
decimalpack <decimalpack@users.noreply.github.com>
2021-06-10 23:08:36 +0530

    Custom implement murmur3

--- src/murmur3.rs
+++ src/murmur3.rs
@@ -1,89 +1,44 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2015 Magnus Hallin
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
-// https://github.com/mhallin/murmurhash3-rs/blob/202fbc5b74859a723d9cbce05d32370eb730411e/src/mmh3_32.rs
-use std::mem;
-
-fn fmix32(mut h: u32) -> u32 {
-    h ^= h >> 16;
-    h = h.wrapping_mul(0x85ebca6b);
-    h ^= h >> 13;
-    h = h.wrapping_mul(0xc2b2ae35);
-    h ^= h >> 16;
-
-    return h;
-}
-
-fn get_32_block(bytes: &[u8], index: usize) -> u32 {
-    let b32: &[u32] = unsafe { mem::transmute(bytes) };
-
-    return b32[index];
-}
-
 pub fn murmurhash3_x86_32(bytes: &[u8], seed: u32) -> u32 {
-    let c1 = 0xcc9e2d51u32;
-    let c2 = 0x1b873593u32;
-    let read_size = 4;
-    let len = bytes.len() as u32;
-    let block_count = len / read_size;
+    // https://en.wikipedia.org/wiki/MurmurHash
+    let c1 = 0xcc9e2d51;
+    let c2 = 0x1b873593;
+    let r1 = 15;
+    let r2 = 13;
+    let m = 5;
+    let n = 0xe6546b64;
 
-    let mut h1 = seed;
-
-    for i in 0..block_count as usize {
-        let mut k1 = get_32_block(bytes, i);
-
-        k1 = k1.wrapping_mul(c1);
-        k1 = k1.rotate_left(15);
-        k1 = k1.wrapping_mul(c2);
-
-        h1 ^= k1;
-        h1 = h1.rotate_left(13);
-        h1 = h1.wrapping_mul(5);
-        h1 = h1.wrapping_add(0xe6546b64)
-    }
-    let mut k1 = 0u32;
-
-    if len & 3 == 3 {
-        k1 ^= (bytes[(block_count * read_size) as usize + 2] as u32) << 16;
-    }
-    if len & 3 >= 2 {
-        k1 ^= (bytes[(block_count * read_size) as usize + 1] as u32) << 8;
-    }
-    if len & 3 >= 1 {
-        k1 ^= bytes[(block_count * read_size) as usize + 0] as u32;
-        k1 = k1.wrapping_mul(c1);
-        k1 = k1.rotate_left(15);
-        k1 = k1.wrapping_mul(c2);
-        h1 ^= k1;
+    if bytes.len() == 0 {
+        return 0;
     }
 
-    h1 ^= bytes.len() as u32;
-    h1 = fmix32(h1);
+    let mut hash: u32 = seed;
 
-    return h1;
+    for four_byte_chunk in bytes.chunks(4) {
+        let mut arr = [0u8; 4];
+        four_byte_chunk
+            .iter()
+            .enumerate()
+            .for_each(|(i, &x)| arr[i] = x);
+        let mut k: u32 = u32::from_le_bytes(arr);
+        k = k.wrapping_mul(c1);
+        k = k.rotate_left(r1);
+        k = k.wrapping_mul(c2);
+
+        hash ^= k;
+        if four_byte_chunk.len() == 4 {
+            hash = hash.rotate_left(r2);
+            hash = hash.wrapping_mul(m).wrapping_add(n);
+        }
+    }
+
+    hash ^= bytes.len() as u32;
+    hash ^= hash >> 16;
+    hash = hash.wrapping_mul(0x85ebca6b);
+    hash ^= hash >> 13;
+    hash = hash.wrapping_mul(0xc2b2ae35);
+    hash ^= hash >> 16;
+    hash
 }
-
 #[cfg(test)]
 mod test {
     use super::murmurhash3_x86_32;
