b9fc2b046d975925f8be9e213ad7cfaaf7e8e832
decimalpack <decimalpack@users.noreply.github.com>
2021-06-13 16:54:28 +0530

    Add CLI

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,4 @@
+use clap::Clap;
 use serde::{Deserialize, Serialize};
 use spectral_bloom_filter::SpectralBloomFilter;
 use std::collections::HashMap;
@@ -19,13 +20,67 @@ struct SearchItem {
     n_hash_functions: u32,
 }
 
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Kevin K. <kbknapp@gmail.com>")]
+struct Opts {
+    /// The tokens.json file from which to read.
+    /// The file has the following structure:
+    ///
+    /// ```json
+    ///  [
+    ///      {
+    ///          "document_link":"doc1_link"
+    ///          "term_frequency": {
+    ///                  // word as key and frequency as value
+    ///                  "word1":1,
+    ///                  "word2":2,
+    ///           }
+    ///      },
+    ///      {
+    ///          "document_link":"doc2_link"
+    ///          "term_frequency": {
+    ///                  "word1":1,
+    ///                  "word2":2,
+    ///           }
+    ///      }
+    ///      // And so on
+    ///  ]
+    /// ```json
+    #[clap(short, long)]
+    tokens_file: String,
+
+    /// Name of the output file
+    #[clap(short, long, default_value = "search.html")]
+    output_file: String,
+
+    /// The false positive rate, lower rate means higher sizes
+    ///
+    /// High values will introduce more false positives in search results
+    #[clap(short, long, default_value = "0.01")]
+    false_positive_rate: f32,
+
+    /// Number of bits to allacote for a single counter
+    /// For width `w` bits, max frequency estimate will be `2^w - 1`
+    ///
+    /// Lower the width, smaller the sizes.
+    ///
+    /// Small values may affect the ranking of documents
+    #[clap(short = 'w', long, default_value = "4")]
+    counter_width: u32,
+}
 fn main() -> std::io::Result<()> {
-    let file = File::open("tokens.json")?;
-    let false_positive_rate = 0.01;
-    let width = 4;
+    // Parse CLI options
+    let opts = Opts::parse();
+
+    let file = File::open(opts.tokens_file)?;
+    let false_positive_rate = opts.false_positive_rate;
+    let width = opts.counter_width;
+
+    // Read file
     let buf_reader = BufReader::new(file);
     let tokens_json: Vec<Document> = serde_json::from_reader(buf_reader)?;
 
+    // Create search index with base2p15 encoding
     let search_index: Vec<SearchItem> = tokens_json
         .into_iter()
         .map(|document| {
@@ -39,8 +94,11 @@ fn main() -> std::io::Result<()> {
             }
         })
         .collect();
+
+    // Write to file using template
+    // Instead of template engine, use string replace as hack
     let j = serde_json::to_string(&search_index)?;
-    let template = std::fs::read_to_string("search_template.html")?;
+    let template = include_str!("search_template.html");
     let search_page = template.replace("UNIQUE_SEARCH_INDEX_PLACEHOLDER", j.as_str());
-    std::fs::write("search.html", search_page)
+    std::fs::write(opts.output_file, search_page)
 }
