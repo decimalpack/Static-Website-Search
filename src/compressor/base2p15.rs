f44b1ba1221ae90825bbd4aea8b8e0f674ec03bb
decimalpack <decimalpack@users.noreply.github.com>
2021-06-15 21:03:56 +0530

    Add decode and tests

--- src/compressor/base2p15.rs
+++ src/compressor/base2p15.rs
@@ -23,3 +23,33 @@ pub fn encode(bit_string: &String) -> String {
         .map(|result| result.unwrap())
         .collect()
 }
+
+pub fn decode(base2p15_encoded: &String) -> String {
+    let offset = 0xa1;
+    let padding_char: char = base2p15_encoded.chars().nth(0).unwrap();
+    let n_padded_bits = padding_char.to_digit(16).unwrap();
+    let mut decoded: String = base2p15_encoded
+        .chars()
+        .skip(1)
+        .map(|c| format!("{:015b}", c as u32 - offset))
+        .collect();
+    (0..n_padded_bits).for_each(|_| {
+        decoded.pop();
+    });
+    decoded
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use proptest::prelude::*;
+    proptest! {
+
+        #[test]
+        fn proptest_reversible(bit_string in "[0-1]*") {
+            println!("{}",bit_string);
+            let s = decode(&encode(&bit_string));
+            prop_assert_eq!(s,bit_string);
+        }
+    }
+}
